ba7dddd6339ae81fa9376111ffb1dea8780300b1
code-logic11235 <tai.pham1738@gmail.com>
2021-06-22 16:39:18 -0600

    refractor forms

--- react_client/src/components/forms/Forms.jsx
+++ react_client/src/components/forms/Forms.jsx
@@ -6,27 +6,9 @@ export default function Forms(){
   return (
     <>
 
-      <div className='hero'>
-        
-        <div className='form-content-left'>
-          <img className='form-img' src = 'https://picsum.photos/500/600' alt='form-img'/>
-        </div>
 
-        <div className='form-content-right'>
-    
-          <div className='form-box'>
-            <div className='button-box'>
-
-              <div id='btn'></div>
-
-              <button type = 'button' className= 'toggle-btn'> Sign In </button>
-              <button type = 'button' className= 'toggle-btn'> Register </button>
-              
-            </div>
             <SignUpForm/>
-          </div>
-        </div>
-      </div>
+
     </>
   )
 }
--- react_client/src/components/forms/SignUpForm.jsx
+++ react_client/src/components/forms/SignUpForm.jsx
@@ -10,9 +10,9 @@ export default function SignUpForm () {
   const [errors, setErrors] = useState({});
   const [firstClickRef, setFirstClickRef] = useState(false);
   const [values, setValues] = useState({
-    username: '',
-    email: '',
-    password: '',
+    username: null,
+    email: null,
+    password: null,
   })
   const handleChange = (e)=>{
     
@@ -72,67 +72,128 @@ export default function SignUpForm () {
   };
 
   return (
-    <>
-      <div className = 'signup-wrapper' >
+    
+      <div className= 'form-wrapper'>
+        <form className = 'form' onSubmit={handleClick}>
+          {/* <img src = '../dist/image/Avatar.png'> </img> */}
+          <h2> Register New Account</h2>
 
-        <form className='form-wrapper' onSubmit={handleClick}>
-          <div className='name'>
-            <label for='username'>Username</label>
+          <div className='input-group'>
             <input type="text" className='txt-input'
             name = 'username'
             value={values.username}  
             onChange={handleChange} 
             autocomplete="off" 
-            placeholder = 'Enter your username'
+            placeholder='.'
+            
             />
+            <label for= 'username'> Username</label>
           </div>
 
-          {(errors.username && firstClickRef) && <p className='validate-error'>{errors.username}</p>}
-
-          <div className = 'email'>
-             <label for='email'>Email</label>
+          <div className='input-group'> 
             <input type="email" className='txt-input' 
             name = 'email'  
             value={values.email}
             onChange={handleChange} 
             autocomplete="off" 
-
-            placeholder = 'Enter your email'
+            placeholder='.'
+            
             /> 
+            <label for= 'email'> Email</label>
           </div>
 
-          {(errors.email && firstClickRef) && <p className='validate-error'>{errors.email}</p>}
 
-          <div className='password'>
-           <label for='password'>Password</label>
-          <input type="password" className='txt-input'
-          name = 'password' 
-          value={values.password}
-          autocomplete="off" 
-          onChange={handleChange} 
-          placeholder = 'Enter your password'
-          />
+
+          <div className='input-group'> 
+            <input type="password" className='txt-input'
+            name = 'password' 
+            value={values.password}
+            autocomplete="off" 
+            onChange={handleChange} 
+            
+            placeholder='.'
+            />
+            <label for= 'password'> Password</label>
           </div>
 
-          {(errors.password && firstClickRef) && <p className='validate-error'>{errors.password}</p>}
 
-          <div className='confirm-password'>
-           <label for='confirmPassword'>Confirm Password</label>
-          <input type="password" className='txt-input'
-          name = 'confirmPassword' 
-          value={values.confirmPassword}
-          autocomplete="off" 
-          onChange={handleChange} 
-          placeholder = 'Enter your password2'
-          />
-          {/* {(errors.password && ref) && <p className='validate-error'>{errors.password}</p>} */}
+          <div className='input-group'> 
+            <input type="password" className='txt-input'
+            name = 'confirmPassword' 
+            value={values.confirmPassword}
+            autocomplete="off" 
+            onChange={handleChange} 
+            
+            placeholder='.'
+            />
+            <label for= 'confirmPassword'> Confirm Password</label>
           </div>
-          {(errors.confirmPassword && firstClickRef) && <p className='validate-error'>{errors.confirmPassword}</p>}
-          <div>
-          <input type="submit" value="Next"   />  
-          </div>
+
+          <input className = 'submit-btn' type="submit" value="signup"   />
+          {/* <hr /> */}
+          <a href='.log-in' id='log-in-instead'>Already have an account?</a>
         </form>
       </div>
-    </>
+
+    
   )
 }
+
+        // <form className='form-wrapper' onSubmit={handleClick}>
+        //   <div className='name'>
+
+        //     <input type="text" className='txt-input'
+        //     name = 'username'
+        //     value={values.username}  
+        //     onChange={handleChange} 
+        //     autocomplete="off" 
+        //     placeholder = 'Enter your username'
+        //     />
+        //   </div>
+
+        //   {(errors.username && firstClickRef) && <p className='validate-error'>{errors.username}</p>}
+
+        //   <div className = 'email'>
+
+            // <input type="email" className='txt-input' 
+            // name = 'email'  
+            // value={values.email}
+            // onChange={handleChange} 
+            // autocomplete="off" 
+
+            // placeholder = 'Enter your email'
+            // /> 
+        //   </div>
+
+        //   {(errors.email && firstClickRef) && <p className='validate-error'>{errors.email}</p>}
+
+        //   <div className='password'>
+
+        //   <input type="password" className='txt-input'
+        //   name = 'password' 
+        //   value={values.password}
+        //   autocomplete="off" 
+        //   onChange={handleChange} 
+        //   placeholder = 'Enter your password'
+        //   />
+        //   </div>
+
+        //   {(errors.password && firstClickRef) && <p className='validate-error'>{errors.password}</p>}
+
+        //   <div className='confirm-password'>
+
+        //   <input type="password" className='txt-input'
+        //   name = 'confirmPassword' 
+        //   value={values.confirmPassword}
+        //   autocomplete="off" 
+        //   onChange={handleChange} 
+        //   placeholder = 'Enter your password2'
+        //   />
+         
+        //   </div>
+        //   {(errors.confirmPassword && firstClickRef) && <p className='validate-error'>{errors.confirmPassword}</p>}
+        //   <div>
+         
+        //   <input type="submit" value="Next"   />  
+        //   </div>
+        // </form>
